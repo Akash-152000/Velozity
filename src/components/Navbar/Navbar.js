6809c8bf81890e2762df1d42c51a3fe488a305c2
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-08-29 19:37:12 +0530

    Optimsed search using debounce

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React,{useCallback, useContext} from 'react'
 import fetchMoviesContext from '../../context/FetchMovies/fetchMoviesContext'
 import './navbar.css'
 
@@ -7,16 +7,30 @@ const Navbar = () => {
   const context = useContext(fetchMoviesContext);
   const {searchMovie} = context;
 
+
+  const debounce = (func) =>{
+    let timer;
+    return function(...args){
+      const context = this;
+      if(timer) clearTimeout(timer)
+      timer = setTimeout(()=>{
+        timer=null
+        func.apply(context,args)
+      },300)
+    }
+  }
+
   const handleChange=(e)=>{
     searchMovie(e.target.value)
   }
+
+  const optimisedVersion = useCallback(debounce(handleChange,[]))
 
   return (
     <div className='navbar'>
         <div className="logo"><h3>Velozity Assignment</h3></div>
         <div className="search">
-            <input className='search-input' type="text" onChange={handleChange} placeholder='Search your favourite movies...'/>
-            {/* <button className='search-button'>Search</button> */}
+            <input className='search-input' type="text" onChange={optimisedVersion} placeholder='Search your favourite movies...'/>
         </div>
     </div>
   )
